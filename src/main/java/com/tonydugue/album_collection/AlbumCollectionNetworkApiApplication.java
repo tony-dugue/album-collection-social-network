ea84f45ee75b497707bacc14a4416456271db5f0
Tony Dugué <duguetony@gmail.com>
2024-08-21 01:59:22 +0200

    init spring boot project

---
+++ src/main/java/com/tonydugue/album_collection/AlbumCollectionNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.tonydugue.album_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCollectionNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCollectionNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonydugue/album_collection/AlbumCollectionNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonydugue.album_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumCollectionNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
