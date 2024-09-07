7fbbd6d62fa0532917b228050a0b996346319c51
Tony Dugué <duguetony@gmail.com>
2024-09-07 13:18:42 +0200

    feat(auth): implement user and role repositories

---
+++ src/main/java/com/tonydugue/album_collection/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.tonydugue.album_collection.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+  Optional<Role> findByName(String role);
+}
