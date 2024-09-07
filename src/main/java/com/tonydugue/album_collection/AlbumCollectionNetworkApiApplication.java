28a4eb4c5a4569c66f8b896da6c5419f98b76838
Tony Dugué <duguetony@gmail.com>
2024-09-07 11:46:25 +0200

    feat(auth): add User entity

--- src/main/java/com/tonydugue/album_collection/AlbumCollectionNetworkApiApplication.java
+++ src/main/java/com/tonydugue/album_collection/AlbumCollectionNetworkApiApplication.java
@@ -2,8 +2,10 @@ package com.tonydugue.album_collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AlbumCollectionNetworkApiApplication {
 
 	public static void main(String[] args) {
