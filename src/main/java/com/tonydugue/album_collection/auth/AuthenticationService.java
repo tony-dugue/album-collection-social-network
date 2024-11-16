3d232084d0d079f1d1e63f3cd45ee4889f8726ec
Tony Dugué <duguetony@gmail.com>
2024-11-16 10:42:42 +0100

    feat(auth): create the authentication controller & prepare register method

---
+++ src/main/java/com/tonydugue/album_collection/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.tonydugue.album_collection.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+  public void register(RegistrationRequest request) {
+  }
+}
