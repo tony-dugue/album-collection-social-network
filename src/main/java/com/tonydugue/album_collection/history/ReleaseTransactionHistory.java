684a215e0d0a67150bd7d68d1413a8ba5d43b053
Tony Dugué <duguetony@gmail.com>
2024-11-18 21:23:26 +0100

    feat(ReleaseTransactionHistory): create ReleaseTransactionHistory entity

---
+++ src/main/java/com/tonydugue/album_collection/history/ReleaseTransactionHistory.java
@@ -0,0 +1,24 @@
+package com.tonydugue.album_collection.history;
+
+import com.tonydugue.album_collection.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ReleaseTransactionHistory extends BaseEntity {
+
+  // user relationship
+  // release relationship
+
+  private boolean returned;
+  private boolean returnedApproved;
+}
